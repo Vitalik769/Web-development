51ae3bbc0050fe5a0e9abdc1323931ffaafc7092
Vitalik769 <93341791+Vitalik769@users.noreply.github.com>
2023-10-22 23:55:18 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,64 @@
-import React from 'react';
-import Header from './components/header';
-import Body from './components/body';
-import Footer from './components/footer';
-import './App.css';
+import React, { useState } from 'react';
+import ProductPage from './Components/MainPage';
+import './Css/MainPage.css';
 
 function App() {
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [comments, setComments] = useState([]);
+  const products = [
+    { id: 1, name: 'Товар 1', description: 'Опис товару 1.' },
+    { id: 2, name: 'Товар 2', description: 'Опис товару 2.' },
+    { id: 3, name: 'Товар 3', description: 'Опис товару 3.' },
+  ];
+
+  const handleProductSelect = (productId) => {
+    const product = products.find((p) => p.id === productId);
+    setSelectedProduct(product);
+    // Зберігаємо коментарі для кожного товару окремо
+    setComments((prevComments) => ({
+      ...prevComments,
+      [productId]: prevComments[productId] || [],
+    }));
+  };
+
+  const handleCommentAdd = (comment) => {
+    if (selectedProduct) {
+      setComments((prevComments) => ({
+        ...prevComments,
+        [selectedProduct.id]: [...(prevComments[selectedProduct.id] || []), comment],
+      }));
+    }
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Body />
-      <Footer />
+    <div className="app">
+      <h1>Виберіть товар:</h1>
+      <ul className="ul-product">
+        {products.map((product) => (
+          <li key={product.id}>
+            <label>
+              <input
+                type="radio"
+                name="product"
+                value={product.id}
+                checked={selectedProduct?.id === product.id}
+                onChange={() => handleProductSelect(product.id)}
+              />
+              {product.name}
+            </label>
+          </li>
+        ))}
+      </ul>
+
+      {selectedProduct && (
+        <ProductPage
+          product={selectedProduct}
+          comments={comments[selectedProduct.id] || []}
+          onCommentAdd={handleCommentAdd}
+        />
+      )}
     </div>
   );
 }
 
 export default App;
-
